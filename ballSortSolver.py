3a4a9c10423cbedfbcb3875977cdc155fa87311e
texdade <davidetessarolo@gmail.com>
2021-09-11 17:52:37 +0200

    Solves incredibly easy puzzles, but otherwise goes too deep into recursion

--- ballSortSolver.py
+++ ballSortSolver.py
@@ -15,7 +15,7 @@ def checkMoves(ballData, blockedMoves, current):
 
     #get the color of the ball when want to move
     color = ballData[current][-1]
-    moves = set()
+    moves = list()
 
     #check all moves
     i = 0 #index of vial
@@ -25,28 +25,34 @@ def checkMoves(ballData, blockedMoves, current):
             i+=1
             continue
 
-        #if not blocked, an empty vial is always a move
         if len(vial) == 0:
             if not (current, i) in blockedMoves:
-                moves.add((current, i))
+                #put the empty vial move at the back, so more meaningful moves can be done before
+                if(not sameColor(ballData[current])):
+                    moves.append((current, i))
         #if there is a ball of the same color and the vial isn't full
         elif color == vial[-1] and len(vial) < 4:
             #check if the move isn't blocked
             if not (current, i) in blockedMoves:
-                moves.add((current, i))
+                moves.insert(0, (current, i))
         
         i+=1
-    
+
     return moves
+
+def sameColor(vial):
+    if(len(set(vial)) == 1):
+        return True
 
 def checkWinCon(ballData):
     for vial in ballData:
         #vials must be either empty or have only balls of the same color
         if(vial != []):
-            color = vial[0]
-            for ball in vial:
-                if ball != color:
+            if(len(vial) == 4):
+                if(not sameColor(vial)):
                     return False
+            else:
+                return False
     
     return True
 
@@ -54,25 +60,34 @@ def checkWinCon(ballData):
 def solve(ballData, blockedMoves, current, fromVial, toVial):
     print(ballData)
 
+    if(current == len(ballData)):
+        current = 0
+
     if(fromVial != -1 and toVial != -1):
         ball = ballData[fromVial].pop()
         ballData[toVial].append(ball)
         blockedMoves.add((toVial, fromVial))
 
-    checkWinCon(ballData)
+        #remove blocked moves in that vial
+        blockedMoves = list(blockedMoves)
+        for blockedMove in blockedMoves:
+            if(blockedMove[0] == fromVial):
+                blockedMoves.remove((blockedMove[0], blockedMove[1]))
+        blockedMoves = set(blockedMoves)
+       
+    if(checkWinCon(ballData)):
+        print(ballData)
+        return True
 
     #check possible moves
     moves = checkMoves(ballData, blockedMoves, current)
 
     #if we don't have moves, check next vial
     if(moves == []):
-        if(solve(ballData, blockedMoves, current+1, -1, -1)):
-            return True
+        return solve(ballData, blockedMoves, current+1, -1, -1)
 
     for move in moves:
-        if(solve(ballData, blockedMoves, current, move[0], move[1])):
-            return True
-
+        return solve(ballData, blockedMoves, current, move[0], move[1])
 
 if __name__ == "__main__":
     if(len(sys.argv) != 2):
